6d6bfb95fabffde712563660ae2fc4c9c4cce7a9
bcruik2 <bcruik2@pdx.edu>
2021-02-21 09:51:46 -0800

    Added flags to report_timing

--- syn/work/genus_startup.tcl
+++ syn/work/genus_startup.tcl
@@ -3,7 +3,7 @@ alias fs "set top_design fifo1_sram"
 alias f "set top_design fifo1"
 alias o "set top_design ORCA_TOP"
 alias e "set top_design ExampleRocketSystem"
-set_db timing_report_fields "delay arrival cell timing_point"
+set_db timing_report_fields "delay arrival cell flags timing_point"
 history keep 100
 set systemTime [clock seconds]
 set timefield  [clock format $systemTime -format %y-%m-%d_%H-%m]
